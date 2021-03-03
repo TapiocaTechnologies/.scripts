b1cda4cc2e5de22f6d3a71d4de72ac14c28b7378
tapiocaPENGUIN <elijah.brunsvold@gmail.com>
2021-03-03 10:57:29 -0600

    Added functions for slave and none slave

--- kubsetup.sh
+++ kubsetup.sh
@@ -3,6 +3,7 @@ function enabled_user(){
   cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
   chown $(id -u):$(id -g) $HOME/.kube/config
 }
+function master_setup(){ 
 echo "Disabling SELinux"
 setenforce 0 
 sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
@@ -35,3 +36,44 @@ echo "Initializing Kubernetes"
 kubeadm init && enabled_user
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 kubectl get nodes && echo "kubernetes cluster created" || echo "kubernetes cluster failed to create" 
+}
+
+function slave_setup(){
+
+echo "Disabling SELinux"
+setenforce 0
+sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
+echo "######################################"
+echo "Making firwall rules"
+firewall-cmd --permanent --add-port=6783/tcp
+firewall-cmd --permanent --add-port=10250/tcp
+firewall-cmd --permanent --add-port=10255/tcp
+firewall-cmd --permanent --add-port=30000-32767/tcp
+firewall-cmd  --reload
+echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
+echo "######################################"
+echo "Creating kubernetes repo"
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+echo "Installing Docker and kuberadm"
+yum install kubeadm docker -y && systemctl enable docker && systemctl start docker && systemctl enable kubelet && systemctl start kubelet
+echo "###########################"
+echo "Disabling swap"
+swapoff -a
+}
+
+case $ in 
+  master)
+    master_setup
+    ;; 
+  slave)
+    slave_setup
+    ;;
+esac
